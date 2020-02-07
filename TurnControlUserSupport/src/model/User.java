640e1eea7a30563d024c735fd93ce9e9bbe12df3
user.name <nicoescol0818@gmail.com>
2020-02-07 08:50:37 -0500

    add User Class according with actual diagram

--- TurnControlUserSupport/src/model/User.java
+++ TurnControlUserSupport/src/model/User.java
@@ -1,5 +1,77 @@
 package model;
 
 public class User {
+	
+	//attributes
+	
+	private String id;
+	private String name;
+	private String lastName;
+	private String address;
+	private String phone;
+	
+	//relations
+	
+	private Turn turn;
 
+	
+	//methods
+	
+	public User(String id, String n, String l) {
+		this.id = id;
+		name = n;
+		lastName = l;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public Turn getTurn() {
+		return turn;
+	}
+
+
+	public void setTurn(Turn turn) {
+		this.turn = turn;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User name " + name + ", lastName=" + lastName + ", with turn=" + turn;
+	}
+	
 }
