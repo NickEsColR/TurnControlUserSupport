d1c33ffe755e405b82e4fe1081c9e09314c6c7d2
user.name <nicoescol0818@gmail.com>
2020-02-07 08:30:20 -0500

    add Turn Class

---
+++ TurnControlUserSupport/src/model/Turn.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Turn {
+	
+	//atributes
+	
+	private String turn;
+	
+	//methods
+	
+	public Turn(String t) {
+		turn = t;
+	}
+
+	public String getTurn() {
+		return turn;
+	}
+
+	public void setTurn(String turn) {
+		this.turn = turn;
+	}
+}
