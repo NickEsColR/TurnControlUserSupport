b1f05fef30544f306caf4864418c45da3c23716c
user.name <nicoescol0818@gmail.com>
2020-02-12 14:25:47 -0500

    switch on Main, case 1 and 2 done

--- TurnControlUserSupport/src/ui/Main.java
+++ TurnControlUserSupport/src/ui/Main.java
@@ -2,6 +2,10 @@ package ui;
 import model.*;
 import java.util.Scanner;
 
+import CustomException.NoUserException;
+import CustomException.UserExistException;
+import customException.*;
+import customException.*;
 
 /**
 * NICOLAS ESTEBAN COLMENARES RUIZ
@@ -18,6 +22,41 @@ public class Main {
 	public Main() {
 		serviceCenter = new ServiceCenter();
 		board = new Scanner(System.in);
+		System.out.println("choose an option");
+		System.out.println("1. Set user a turn");
+		System.out.println("2. Add a new user");
+		System.out.println("3.advanceTurn");
+		System.out.println("4. add phone");
+		System.out.println("5. add address");
+		System.out.println("6. exit");
+		int option = board.nextInt();
+		board.nextLine();
+		while(option != 6) {
+			switch(option) {
+			case 1:
+				try {
+					System.out.println("Digit the user큦 identification");
+					String id = board.nextLine();
+					serviceCenter.assignUserTurn(id);
+				}catch(NoUserException e) {
+					System.out.println(e.getMessage() + "please add a new user");
+				}
+			break;
+			case 2:
+				try {
+					System.out.println("Digit the user큦 identification");
+					String id = board.nextLine();
+					System.out.println("Digit the user큦 name");
+					String name = board.nextLine();
+					System.out.println("Digit the user큦 last name");
+					String ln = board.nextLine();
+					serviceCenter.addUser(id, name, ln);
+				}catch(UserExistException e) {
+					System.out.println(e.getMessage());
+				}
+			break;
+			}
+		}
 	}
 	public static void main(String[]args) {
 		Main m = new Main();
