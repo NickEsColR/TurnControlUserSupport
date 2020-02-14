57a9f1b61994116b4ff3e80657ce9021a40dde61
user.name <nicoescol0818@gmail.com>
2020-02-13 21:24:21 -0500

    add test classes to class diagram

---
+++ TurnControlUserSupport/src/TestCase/ServiceCenterTest.java
@@ -0,0 +1,27 @@
+package TestCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+import model.*;
+import CustomException.*;
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+class ServiceCenterTest  extends TestCase{
+	
+	private ServiceCenter sc;
+	
+	private void setup1() {
+		sc = new ServiceCenter();
+		try {
+			sc.addUser("123456789",  "Benito", "Kmelapija");
+		}catch(UserExistException e) {
+			
+		}
+	}
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
