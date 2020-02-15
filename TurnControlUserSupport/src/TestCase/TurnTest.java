ffb872f36e4223b6c11413ed365ecdedf4755e82
user.name <nicoescol0818@gmail.com>
2020-02-15 14:10:36 -0500

    create UserTest class

--- TurnControlUserSupport/src/TestCase/TurnTest.java
+++ TurnControlUserSupport/src/TestCase/TurnTest.java
@@ -5,11 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/**
+* NICOLAS ESTEBAN COLMENARES RUIZ
+* UNIVERSIDAD ICESI
+* ALGORITMOS Y PROGRAMACION 2
+* LABORATORIO 1 TurnControl 
+* 06/02/2020
+*/
+
 public class TurnTest extends TestCase {
 	private Turn turn;
 	
 	@Test
-	void TurnTest() {
+	void Turn() {
 		turn = new Turn('A',11,1);
 		assertFalse("the letter given and register isnt the same", 'A'==turn.getLetter());
 		assertFalse("the number given and register isnt the same", 11 ==turn.getNum());
